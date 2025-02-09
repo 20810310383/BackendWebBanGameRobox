f88de3f6c4e54e44595dec33ac7b003374248388
Do Khac Tu <dokhactu1822@gmail.com>
2025-02-09 23:28:10 +0700

    upp

--- src/routes/giaRobuxRouter.js
+++ src/routes/giaRobuxRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { findOneGiaRobux, createRobux, updateRobux } = require("../controllers/GiaRobux/giaRobux.controller");
+const { findThongBaoBanner, createThongBaoBanner, updateThongBaoBanner } = require("../controllers/GiaRobux/thongBao.controller");
 
 const router = express.Router();
 
@@ -8,6 +9,13 @@ router.get("/get-giarobux", findOneGiaRobux);
 router.post("/create-giarobux", createRobux);
 
 router.put("/update-giarobux", updateRobux);
+
+
+router.get("/get-thong-bao-banner", findThongBaoBanner);
+
+router.post("/create-thong-bao-banner", createThongBaoBanner);
+
+router.put("/update-thong-bao-banner", updateThongBaoBanner);
 
 
 module.exports = router;
