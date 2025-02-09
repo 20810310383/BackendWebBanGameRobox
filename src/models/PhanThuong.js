e2fe994fb2fb8e581e9ff2bdd0be0b32cd8afc62
Do Khac Tu <dokhactu1822@gmail.com>
2025-02-09 21:38:15 +0700

    up

--- src/models/PhanThuong.js
+++ src/models/PhanThuong.js
@@ -4,7 +4,7 @@ const PhanThuong_Schema = new mongoose.Schema({
         code: { type: String},           
         phanThuong: { type: Number},           
         thoiGianHetHan: { type: String},  
-        rate: { type: Number, required: true, min: 1, max: 100 },  // Tỷ lệ phần thưởng (1% đến 100%)
+        rate: { type: Number,  min: 1, max: 100 },  // Tỷ lệ phần thưởng (1% đến 100%)
     },
     { 
         timestamps: true,   // createAt, updateAt
